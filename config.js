876ea5b2710758471583ae28b1111340e5d0b32f
Saikat1462 <77276401+Saikat1462@users.noreply.github.com>
2021-08-25 17:10:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig={
+    apiKey: "AIzaSyAjV2chJpQZcDWsg-phoWjxRRXiApxnIYE",
+    authDomain: "story-telling-app-42477.firebaseapp.com",
+    projectId: "story-telling-app-42477",
+    storageBucket: "story-telling-app-42477.appspot.com",
+    messagingSenderId: "24040067633",
+    appId: "1:24040067633:web:1bedec29f02dd18aa7fb10"
+}
